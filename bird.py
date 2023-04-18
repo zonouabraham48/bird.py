5d7bce324440643ca19a7dbd803b592469082658
zonouabraham48 <129785108+zonouabraham48@users.noreply.github.com>
2023-04-18 18:28:23 +0300

    Create bird.py

---
+++ bird.py
@@ -0,0 +1,31 @@
+class Bird:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def fly(self):
+        pass
+
+
+class Pigeon(Bird):
+    def __init__(self, name):
+        super().__init__(name, species="Pigeon")
+
+    def fly(self):
+        return "I am flying like a pigeon!"
+
+
+class Eagle(Bird):
+    def __init__(self, name):
+        super().__init__(name, species="Eagle")
+
+    def fly(self):
+        return "I am flying like an eagle!"
+
+
+class Ostrich(Bird):
+    def __init__(self, name):
+        super().__init__(name, species="Ostrich")
+
+    def fly(self):
+        return "I cannot fly, I am an ostrich."
